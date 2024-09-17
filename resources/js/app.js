d8d5ad5041972a5e7aa822a6834ad40b00102b68
unknown <2120100443@soy.utj.edu.mx>
2024-09-17 04:51:12 -0600

    mi proyecto estadia2024

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App1';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Estadia 2024</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="root"></div>
+      <h1>Hello World</h1>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
